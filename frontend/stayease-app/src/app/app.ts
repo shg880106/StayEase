6e6c77940dfab92432595c4f256f45ba22786842
Saily Hurtado <sailyhurtado@gmail.com>
2026-04-21 18:51:17 +0200

    feat: add footer component and integrate into app layout
    Created FooterComponent with footer.html and footer.ts to display app branding and copyright.
    Integrated <app-footer /> into app.html for global visibility.
    Updated app.ts to import and declare FooterComponent.
    Minor formatting adjustment in home.html.

--- frontend/stayease-app/src/app/app.ts
+++ frontend/stayease-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
