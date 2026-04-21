7d3ddb21a27a80bb61b82ab5118602c8f2fb41fe
Saily Hurtado <sailyhurtado@gmail.com>
2026-04-21 14:10:36 +0200

    feat: add migration AddImageUrlToProperty entity

--- backend/src/StayEaseApp.Infrastructure/Migrations/20260421115955_AddImageUrlToProperty.Designer.cs
+++ backend/src/StayEaseApp.Infrastructure/Migrations/20260421115955_AddImageUrlToProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StayEaseApp.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using StayEaseApp.Infrastructure.Persistence;
 namespace StayEaseApp.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260421115955_AddImageUrlToProperty")]
+    partial class AddImageUrlToProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
